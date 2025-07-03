4d1b37342fe5bb4fc76d6af67c1b4d8ca004c77c
Aastha Pant <pantashthapant03@gmail.com>
2025-07-03 19:25:56 +0000

    dates

---
+++ 01_basics/dates.js
@@ -0,0 +1,19 @@
+//From January ! 1970
+/*let myDate = new Date()
+console.log(myDate);
+//string conversion
+console.log(myDate.toString());
+
+console.log(myDate.toDateString());
+
+console.log(myDate.toLocaleString());
+
+console.log(myDate.toLocaleDateString());
+
+console.log(myDate.toJSON());
+
+console.log(typeof myDate); // type of date is object it is an object
+*/
+
+let mycreate = new Date(2023 , 0 , 23)
+console.log(mycreate.toDateString());
